f49d430709a8022584d49c3d4b91d3385daed0ff
EmAdthasit <66411403+EmAdthasit@users.noreply.github.com>
2020-11-28 14:31:43 +0700

    material

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,19 +1,26 @@
-import React from 'react';
-import './App.css';
-import { useNuiService } from './nui-events/hooks/useNuiService';
+import React from "react";
+import "./App.css";
+import { useNuiService } from "./nui-events/hooks/useNuiService";
 
 // show and hide
-import { useVisibility } from './core/hooks/useVisibility';
-import { useCoreService } from './core/hooks/useCoreService';
+import { useVisibility } from "./core/hooks/useVisibility";
+import { useCoreService } from "./core/hooks/useCoreService";
+
+// material\
+import { Container } from "@material-ui/core";
 
 function App() {
-  const { visibility } = useVisibility()
+  const { visibility } = useVisibility();
   useNuiService();
   useCoreService();
   return (
-    <div style={ visibility ? { visibility: 'visible' } : { visibility: 'hidden' }}>
-     {/** Any types of components goes here. Maybe some routing? */}
-     <h1>Hello World</h1>
+    <div
+      style={visibility ? { visibility: "visible" } : { visibility: "hidden" }}
+    >
+      {/** Any types of components goes here. Maybe some routing? */}
+      <Container maxWidth="sm">
+        <h1>Hello World</h1>
+      </Container>
     </div>
   );
 }

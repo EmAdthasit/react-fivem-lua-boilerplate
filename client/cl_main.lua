b9db0f5cde71d7253a791248ba57785abfb8211c
EmAdthasit <66411403+EmAdthasit@users.noreply.github.com>
2020-11-28 13:57:08 +0700

    Update cl_main.lua

--- client/cl_main.lua
+++ client/cl_main.lua
@@ -1,9 +1,17 @@
 -- Example of how it works. Look at the `useCoreService`, and the nui function in `nui-events`
 
 RegisterCommand('show:nui', function(source, args, rawCommand)
-  SendNuiMessage({
+  SendNUIMessage({
     app = "REACTNUI",
     method = "setVisibility",
     data = true
+  })
+end, false)
+
+RegisterCommand('hide:nui', function(source, args, rawCommand)
+  SendNUIMessage({
+    app = "REACTNUI",
+    method = "setVisibility",
+    data = false
   })
 end, false)

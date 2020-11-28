7c135330a4b2886dbdb5b260b48c3c44b1bf7ba3
EmAdthasit <66411403+EmAdthasit@users.noreply.github.com>
2020-11-28 13:57:14 +0700

    Update state.ts

--- web/src/core/hooks/state.ts
+++ web/src/core/hooks/state.ts
@@ -1,8 +1,8 @@
-import { atom } from 'recoil';
+import { atom } from "recoil";
 
 export const coreState = {
   visibility: atom({
-    key: 'coreStateHidden',
-    default: false
-  })
-}
+    key: "coreStateHidden",
+    default: false,
+  }),
+};
